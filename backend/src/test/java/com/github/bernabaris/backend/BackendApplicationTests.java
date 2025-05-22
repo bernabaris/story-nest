323d44920c0a27baa8587eb58c7ad266c1bf701d
bernabaris <bernabaris1 @gmail.com>
2025-05-22 21:28:52 +0300

    init code

---
+++ backend/src/test/java/com/github/bernabaris/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bernabaris.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
